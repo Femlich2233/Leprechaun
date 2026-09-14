6b9cdf22a6b987c99b6f3c1cb4eb4b83d5329880
Femlich2233 <stevensegway3@gmail.com>
2026-09-14 04:45:00 -0600

    Update sealed arcanist.lua

--- src/Jokers/sealed arcanist.lua
+++ src/Jokers/sealed arcanist.lua
@@ -7,28 +7,34 @@ SMODS.Joker {
     },
     rarity = 3,
     cost = 10,
-    config = { 
-        extra = 
+    config = {
+        extra =
         {
-             odds = 4, card_limit = 2
-            }
-        },
+            odds = 4, card_limit = 2
+        }
+    },
     loc_vars = function(self, info_queue, card)
-          local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds,
+        local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds,
             'Arcanist_Spectrals')
-        return {vars = {numerator, denominator, card.ability.extra.card_limit}}
+        return { vars = { numerator, denominator, card.ability.extra.card_limit } }
     end,
 
     add_to_deck = function(self, card, from_debuff)
-        G.E_MANAGER:add_event(Event({func = function()
-            G.consumeables.config.card_limit = G.consumeables.config.card_limit + card.ability.extra.card_limit
-            return true end }))
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                G.consumeables.config.card_limit = G.consumeables.config.card_limit + card.ability.extra.card_limit
+                return true
+            end
+        }))
     end,
 
     remove_from_deck = function(self, card, from_debuff)
-        G.E_MANAGER:add_event(Event({func = function()
-            G.consumeables.config.card_limit = G.consumeables.config.card_limit - card.ability.extra.card_limit
-            return true end }))
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                G.consumeables.config.card_limit = G.consumeables.config.card_limit - card.ability.extra.card_limit
+                return true
+            end
+        }))
     end
 
 }
